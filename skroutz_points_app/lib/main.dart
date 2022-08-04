374cddeb432a211dacddfb7bce4566e06ed36a76
Nagas <n.agas@skroutzdelivery.gr>
2022-08-04 12:57:27 +0300

    main_view

--- skroutz_points_app/lib/main.dart
+++ skroutz_points_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skroutz_points_app/pages/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Skroutz points locator',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,10 +25,11 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainAppPage(),
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
