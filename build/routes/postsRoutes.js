53bbae359746c54ec6e5fa8834b79ce047552021
Nagas <n.agas@skroutzdelivery.gr>
2022-08-04 20:12:47 +0300

    added tsconfig file

---
+++ build/routes/postsRoutes.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/** source/routes/posts.ts */
+const express_1 = __importDefault(require("express"));
+const postsController_1 = __importDefault(require("../controllers/postsController"));
+const router = express_1.default.Router();
+router.get("/posts", postsController_1.default.getPosts);
+router.get("/posts/:id", postsController_1.default.getPost);
+router.put("/posts/:id", postsController_1.default.updatePost);
+router.delete("/posts/:id", postsController_1.default.deletePost);
+router.post("/posts", postsController_1.default.addPost);
+exports.default = router;
